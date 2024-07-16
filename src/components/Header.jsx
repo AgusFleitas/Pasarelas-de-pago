499ccd0c388920f2ca70eb22aea1cc09a36b4758
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-07-16 20:22:04 +0200

    Change w-full by max-w

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Cart from "./Cart";
 
 const Header = () => {
   return (
-    <header className='relative border-2 border-emerald-600 rounded-md flex w-full justify-end items-center py-2 px-36 mb-8 h-12'>
+    <header className='relative border-2 border-emerald-600 rounded-md flex mt-8 max-w-[1400px] justify-end items-center py-2 px-36 mb-8 h-12 mx-auto'>
       <div>
         <Cart />
       </div>
