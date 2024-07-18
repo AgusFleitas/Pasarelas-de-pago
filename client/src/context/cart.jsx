906fc3321145ac660d6ee606e4dcc16531b832a7
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-07-18 18:06:19 +0200

    Client-side with MercadoPago supposed to be working

--- client/src/context/cart.jsx
+++ client/src/context/cart.jsx
@@ -1,13 +1,18 @@
 import { createContext, useReducer } from "react";
 import { catalogue } from "../js/catalogue";
 
+import axios from 'axios'
+
 // 1. Crear contexto.
 export const CartContext = createContext();
 
 // REDUCER.
 
-// Creamos el estado inicial para el reducer, en este caso, el carrito vacío.
-const initialState = [];
+// Creamos el estado inicial para el reducer, en este caso tendremos dos propiedades: 'cart' que será un array vacío para gestionar el carrito y 'preferenceId' para gestionar la preferencia una vez se confirme la compra.
+const initialState = {
+  cart: [],
+  preferenceId: null,
+};
 // Creamos el reducer. Va a recibir el estado y la acción que debe ejecutar.
 const reducer = (state, action) => {
   // De la acción que recibe el reducer, vamos a sacar el 'tipo' de acción y el 'payload', que en nuestro caso será la información del producto.
@@ -19,40 +24,52 @@ const reducer = (state, action) => {
       const { id } = actionPayload;
 
       // Buscamos si el producto ya se encuentra en el carrito.
-      const productInCartIndex = state.findIndex((item) => item.id === id);
+      const productInCartIndex = state.cart.findIndex((item) => item.id === id);
 
       // Si ya se encuentra en el carrito, creamos una copia profunda del carrito con structuredCLone, luego accedemos a la propiedad 'quantity' de ese producto y la incrementamos en 1. Finalmente, retornamos nuestra copia modificada.
       if (productInCartIndex >= 0) {
-        const newState = structuredClone(state);
+        const newState = structuredClone(state.cart);
         newState[productInCartIndex].quantity += 1;
-        return newState;
+        return { ...state, cart: newState };
       }
 
       // Si no está en el carrito vamos a crear una constante llamada newItem para insertar el nuevo producto, este va a ser el retorno de encontrar en el catálogo el producto con el ID que recibimos en el Payload. Luego retornamos una copia del estado (para conservar los productos que ya tenemos), una copia de nuestro item y la cantidad en 1.
 
       const newItem = catalogue.find((item) => item.id === id);
 
-      return [
+      return {
         ...state,
-        {
-          ...newItem,
-          quantity: 1,
-        },
-      ];
+        cart: [
+          ...state.cart,
+          {
+            ...newItem,
+            quantity: 1,
+          },
+        ],
+      };
     }
 
     case "REMOVE_FROM_CART": {
       const { id } = actionPayload;
+      const filteredCart = state.cart.filter((item) => item.id != id)
 
-      return state.filter((item) => item.id != id);
+      return {
+        ...state,
+        cart: filteredCart
+      }
     }
 
     case "CLEAR_CART": {
       return initialState;
     }
-  }
 
-  return state;
+    case "SET_PREFERENCE_ID": {
+      return { ...state, preferenceId: actionPayload };
+    }
+
+    default:
+      return state;
+  }
 };
 
 // 2. Crear Provider (este archivo) y proveer el contexto (en donde sea necesario).
@@ -74,13 +91,48 @@ export function CartProvider({ children }) {
 
   const clearCart = () => dispatch({ type: "CLEAR_CART" });
 
+  // Funcion para crear la preferencia.
+  const createPreference = async () => {
+    const cartItems = state.cart.map((product) => {
+      return {
+        name: product.titulo,
+        quantity: product.quantity,
+        price: product.precio,
+        currency: "ARS",
+        description: product.descripcion,
+        image: product.imagen,
+      };
+    });
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/create-preference",
+        {
+          products: cartItems,
+        }
+      );
+
+      const { id } = response.data;
+      dispatch({
+        type: 'SET_PREFERENCE_ID',
+        payload: id
+      })
+      return id;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
   return (
     <CartContext.Provider
       value={{
-        cart: state,
+        cart: state.cart,
         addToCart,
         removeFromCart,
         clearCart,
+        createPreference,
+        preferenceId: state.preferenceId
       }}
     >
       {children}
--- client/src/views/ConfirmAndPay.jsx
+++ client/src/views/ConfirmAndPay.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 
-import axios from "axios";
 import useCart from "../hooks/useCart";
 
 // Se inicializa MercadoPago FUERA del componente para evitar múltiples inicializaciones por si el componente se vuelve a renderizar.
@@ -12,67 +11,32 @@ initMercadoPago(publicKey, {
 
 const ConfirmAndPay = () => {
   // Traer el carrito del contexto con el Hook.
-  const { cart } = useCart();
+  const { cart, createPreference, preferenceId } = useCart();
 
-  // Estado local para almacenar el ID de la preferencia.
-  const [preferenceId, setPreferenceId] = useState(null);
-  
-  // Estado local para almacenar un posible error al obtener el ID de la preferencia.
-  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // Función que se va a ejecutar con el useEffect para solicitar la preferencia al servidor.
-  const createPreference = async () => {
-    console.log('Se ejecuta el createPreference');
-    const cartItems = cart.map((product) => {
-      return {
-        name: product.titulo,
-        quantity: product.quantity,
-        price: product.precio,
-        currency: "ARS",
-        description: product.descripcion,
-        image: product.imagen,
-      };
-    });
-
-    try {
-      const response = await axios.post(
-        "http://localhost:3000/create-preference",
-        {
-          products: cartItems,
-        }
-      );
-
-      const { id } = response.data;
-      return id;
-    } catch (error) {
-      console.error(error);
-      setError(
-        "Hubo un problema al crear la preferencia de pago. Inténtalo de nuevo."
-      );
-      return null;
-    }
-  };
+  // Para ejecutar una función asíncrona (createPreference()) dentro del useEffect debemos crear otra función asíncrona que la ejecute y que la espere, además, debemos ejecutarla al final del useEffect. Por eso creamos 'getPreferenceId()' para ejecutar 'createPreference()' dentro de ella y al final del useEffect ejecutamos 'getPreferenceId()'.
 
   // useEffect para obtener la preferencia tan pronto se monte el componente.
-  // Para ejecutar una función asíncrona (createPreference()) dentro del useEffect debemos crear otra función asíncrona que la ejecute y que la espere, además, debemos ejecutarla al final del useEffect. Por eso creamos 'getPreferenceId()' para ejecutar 'createPreference()' dentro de ella y al final del useEffect ejecutamos 'getPreferenceId()'.
-//   useEffect(() => {
-//     const getPreferenceId = async () => {
-//         console.log('Se ejecuta el getPreferenceId');
-//       const id = await createPreference();
-//       if (id) {
-//         setPreferenceId(id);
-//       }
-//     };
+  useEffect(() => {
+    const getPreferenceId = async () => {
+      await createPreference(cart);
+      setIsLoading(false);
+    };
 
-//     getPreferenceId();
-//   }, [cart]);
+    getPreferenceId();
+  }, [createPreference, cart]);
 
   return (
     <section>
-      <Wallet
-        initialization={{ preferenceId: preferenceId }}
-        customization={{ texts: { valueProp: "smart_option" } }}
-      />
+      {isLoading ? (
+        <p>Cargando...</p>
+      ) : (
+        <Wallet
+          initialization={{ preferenceId }}
+          customization={{ texts: { valueProp: "smart_option" } }}
+        />
+      )}
     </section>
   );
 };
