08992b86358bb6a4f83d92bb66ef5eb37cbae171
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-07-23 21:35:01 +0200

    Add 'Catalogue' button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,17 @@
 import Cart from "./Cart";
 
+import { Link } from 'react-router-dom'
+
 const Header = () => {
   return (
-    <header className='relative border-2 border-emerald-600 rounded-md flex mt-8 max-w-[1400px] justify-end items-center py-2 px-36 mb-8 h-12 mx-auto'>
-      <div>
+    <header className='border-2 border-emerald-600 rounded-md flex gap-x-2 mt-8 max-w-[1400px] justify-end items-center py-2 px-2 mb-8 h-12 mx-auto'>
+        <Link 
+        to={'/'}
+        className="bg-emerald-800/60 rounded-md cursor-pointer flex h-8 justify-center py-1 px-3 transition-all z-20 hover:bg-emerald-700 font-semibold"
+        >
+          Ir al catálogo
+        </Link>
         <Cart />
-      </div>
     </header>
   );
 };
