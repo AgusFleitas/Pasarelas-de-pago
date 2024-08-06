974f37e01cab1b813b7cfd060c22e8cace927032
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-08-06 21:12:38 +0200

    Change Header style + Delete CSS file

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,14 +1,12 @@
 import { Link, useLocation } from "react-router-dom";
 
-import CartItem from "./CartItem";
 import useCart from "../hooks/useCart";
-import "./Cart.css";
 
 const Cart = () => {
   const location = useLocation();
 
-  // Traemos el carrito y los métodos del Custom Hook.
-  const { cart, clearCart, addToCart, removeFromCart } = useCart();
+  // Traemos el carrito para utilizarlo y conocer la cantidad de productos en él.
+  const { cart } = useCart();
 
   return (
     <>
@@ -16,7 +14,7 @@ const Cart = () => {
         <Link
           title='Ir al carrito.'
           to={"/pay-cart"}
-          className='cart-button relative items-center bg-emerald-800/60 rounded-md cursor-pointer flex h-8 justify-center py-1 px-3 transition-all z-20 hover:bg-emerald-700'
+          className='cart-button relative items-center bg-white rounded-full cursor-pointer flex justify-center py-1 px-3 transition-all z-20 hover:bg-yellow-400'
         >
           <svg
             xmlns='http://www.w3.org/2000/svg'
@@ -30,7 +28,7 @@ const Cart = () => {
             <path d='M6 2a1 1 0 0 1 .993 .883l.007 .117v1.068l13.071 .935a1 1 0 0 1 .929 1.024l-.01 .114l-1 7a1 1 0 0 1 -.877 .853l-.113 .006h-12v2h10a3 3 0 1 1 -2.995 3.176l-.005 -.176l.005 -.176c.017 -.288 .074 -.564 .166 -.824h-5.342a3 3 0 1 1 -5.824 1.176l-.005 -.176l.005 -.176a3.002 3.002 0 0 1 1.995 -2.654v-12.17h-1a1 1 0 0 1 -.993 -.883l-.007 -.117a1 1 0 0 1 .883 -.993l.117 -.007h2zm0 16a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm11 0a1 1 0 1 0 0 2a1 1 0 0 0 0 -2z' />
           </svg>
 
-          <span className='cart-quantity'>{cart.length}</span>
+          <span className='bg-yellow-400 absolute -top-2 -right-2 size-5 text-center font-bold rounded-full flex justify-center items-center'>{cart.length}</span>
         </Link>
       )}
     </>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header className='border-2 border-emerald-600 rounded-md flex gap-x-2 mt-8 max-w-[1400px] justify-end items-center py-2 px-2 mb-8 h-12 mx-auto'>
+    <header className='bg-sky-200/60 rounded-full flex gap-x-2 mt-5 max-w-[1400px] justify-end items-center py-2 px-4 mb-8 h-12 mx-auto shadow-md shadow-gray-400'>
         <Link 
         to={'/'}
-        className="bg-emerald-800/60 rounded-md cursor-pointer flex h-8 justify-center py-1 px-3 transition-all z-20 hover:bg-emerald-700 font-semibold"
+        className="bg-white rounded-full cursor-pointer flex justify-center items-center px-4 h-8 transition-all z-20 hover:bg-yellow-400 font-semibold"
         >
-          Ir al catálogo
+          Catálogo
         </Link>
         <Cart />
     </header>
