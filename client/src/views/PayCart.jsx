8e620e2e72aa6855e4152ec3e5f29e45bc5b19da
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-08-07 20:14:22 +0200

    Re-styling PayCart view + Add Modal for payment methods

--- client/src/views/PayCart.jsx
+++ client/src/views/PayCart.jsx
@@ -3,8 +3,17 @@ import { Link } from "react-router-dom";
 import CartItem from "../components/CartItem";
 import useCart from "../hooks/useCart";
 
+import "./PayCart.css";
+import { useEffect } from "react";
+
 const PayCart = () => {
-  const { cart, addToCart, removeFromCart } = useCart();
+
+  const { cart, addToCart, removeFromCart, clearPreferenceId } = useCart();
+
+  // useEffect para limpiar el preferenceId por si el usuario cancela y vuelve a querer pagar.
+  useEffect(() => {
+    clearPreferenceId();
+  }, [])
 
   let totalPrice = 0;
 
@@ -13,10 +22,26 @@ const PayCart = () => {
     totalPrice += product.precio * product.quantity;
   });
 
+  function openModal() {
+    const modal = document.getElementById("payment-modal");
+    modal.showModal();
+  }
+
+  function closeModal() {
+    const modal = document.getElementById("payment-modal");
+    modal.close();
+  }
+
   return (
-    <section className='flex flex-col'>
-      <div className='flex justify-around px-24'>
-        <ul className='flex flex-col gap-y-4  '>
+    <section className='flex flex-col justify-center'>
+      <h1 className='font-bold text-4xl text-center mb-1'>Tu carrito</h1>
+      <h2 className='text-center text-balance mb-8 max-w-[72rem] self-center'>
+        Verifica los productos en tu carrito, puedes agregar unidades
+        adicionales de cualquier producto o eliminarlos del carrito. Luego
+        presiona 'Confirmar y pagar' y selecciona tu método de pago favorito.
+      </h2>
+      <div className='flex w-full justify-center gap-x-20'>
+        <ul className='flex flex-col gap-y-4'>
           {cart.map((product) => (
             <CartItem
               key={product.id}
@@ -26,15 +51,50 @@ const PayCart = () => {
             />
           ))}
         </ul>
-        <aside className='flex flex-col gap-y-2 border-2 border-black rounded-md py-4 px-6 w-96 h-[32rem]'>
+        <aside className='flex flex-col gap-y-2 border-2 border-black bg-white shadow-md shadow-gray-400 rounded-md py-4 px-6 w-96 h-[32rem]'>
           <h4 className='font-semibold text-2xl text-center'>Resumen</h4>
           <div className='w-full flex justify-between text-lg'>
             <p>Total por productos:</p>
             <span>${totalPrice.toFixed(2)}</span>
           </div>
-          {cart.length > 0 && (
-            <div className='mt-auto flex flex-col gap-y-2'>
-              <h4 className='font-semibold text-center'>Quiero pagar con:</h4>
+          <div className='w-full flex justify-between text-lg'>
+            <p>Gastos de envío:</p>
+            <span>$00.00</span>
+          </div>
+          <div className='w-full flex justify-between text-lg'>
+            <p>Costos adicionales:</p>
+            <span>$00.00</span>
+          </div>
+          <div className='w-full mt-auto font-semibold flex justify-between text-lg'>
+            <p>Total a pagar:</p>
+            <span>${totalPrice.toFixed(2)}</span>
+          </div>
+          <button
+            title='Proceder con la compra y elegir método de pago.'
+            className='w-full mt-2 bg-sky-300/60 rounded-md py-2 font-semibold hover:bg-yellow-400 hover:scale-105 transition-transform'
+            onClick={openModal}
+          >
+            Confirmar y pagar
+          </button>
+        </aside>
+        <dialog
+          id='payment-modal'
+          className='relative w-[42rem] h-72 m-auto rounded-md shadow-lg shadow-black'
+        >
+          <div className='size-full flex flex-col px-6'>
+            <strong className='text-center text-xl my-6'>
+              Elige un método de pago
+            </strong>
+            <button
+              title='Cerrar y volver.'
+              id='close-modal'
+              className='absolute text-xl font-black text-black top-4 right-6 hover:scale-125 hover:text-red-500 transition-transform'
+              onClick={closeModal}
+            >
+              ✖
+            </button>
+            <div className='flex flex-col gap-y-2'>
+              <span className='text-center'>Quiero pagar con:</span>
               <Link
                 to='/confirm-and-pay?gateway=mp'
                 className='text-center font-semibold py-2 rounded-md text-white bg-[#00B1EA] hover:scale-105 transition-transform'
@@ -54,8 +114,8 @@ const PayCart = () => {
                 Stripe
               </Link>
             </div>
-          )}
-        </aside>
+          </div>
+        </dialog>
       </div>
     </section>
   );
