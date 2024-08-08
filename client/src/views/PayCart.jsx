9fd5aa586fb91e829634d12667e41c9e23ef361e
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-08-08 20:25:28 +0200

    Add CartEmpty warning + Disable pay button if the cart is empty

--- client/src/views/PayCart.jsx
+++ client/src/views/PayCart.jsx
@@ -1,19 +1,23 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import CartItem from "../components/CartItem";
 import useCart from "../hooks/useCart";
 
+import emptycart from "../img/empty-cart.png";
 import "./PayCart.css";
-import { useEffect } from "react";
 
 const PayCart = () => {
-
   const { cart, addToCart, removeFromCart, clearPreferenceId } = useCart();
+
+  const [cartEmpty, setCartEmpty] = useState(true);
 
   // useEffect para limpiar el preferenceId por si el usuario cancela y vuelve a querer pagar.
   useEffect(() => {
     clearPreferenceId();
-  }, [])
+
+    cart.length > 0 ? setCartEmpty(false) : setCartEmpty(true);
+  }, [cart.length]);
 
   let totalPrice = 0;
 
@@ -41,16 +45,30 @@ const PayCart = () => {
         presiona 'Confirmar y pagar' y selecciona tu método de pago favorito.
       </h2>
       <div className='flex w-full justify-center gap-x-20'>
-        <ul className='flex flex-col gap-y-4'>
-          {cart.map((product) => (
-            <CartItem
-              key={product.id}
-              addToCart={() => addToCart(product)}
-              removeFromCart={() => removeFromCart(product)}
-              {...product}
+        {cart.length > 0 ? (
+          <ul className='flex flex-col gap-y-4'>
+            {cart.map((product) => (
+              <CartItem
+                key={product.id}
+                addToCart={() => addToCart(product)}
+                removeFromCart={() => removeFromCart(product)}
+                {...product}
+              />
+            ))}
+          </ul>
+        ) : (
+          <div className='max-w-[40rem] flex flex-col justify-center'>
+            <span className='text-4xl font-bold text-zinc-500/40 text-center text-balance  leading-snug'>
+              ¡Tu carrito está vacío! Explora el catálogo en la página de
+              inicio.
+            </span>
+            <img
+              className='size-72 object-contain self-center grayscale opacity-50'
+              src={emptycart}
+              alt='Carrito de compras vacío con una mosca por encima'
             />
-          ))}
-        </ul>
+          </div>
+        )}
         <aside className='flex flex-col gap-y-2 border-2 border-black bg-white shadow-md shadow-gray-400 rounded-md py-4 px-6 w-96 h-[32rem]'>
           <h4 className='font-semibold text-2xl text-center'>Resumen</h4>
           <div className='w-full flex justify-between text-lg'>
@@ -59,20 +77,25 @@ const PayCart = () => {
           </div>
           <div className='w-full flex justify-between text-lg'>
             <p>Gastos de envío:</p>
-            <span>$00.00</span>
+            <span>$0</span>
           </div>
           <div className='w-full flex justify-between text-lg'>
             <p>Costos adicionales:</p>
-            <span>$00.00</span>
+            <span>$0</span>
           </div>
           <div className='w-full mt-auto font-semibold flex justify-between text-lg'>
             <p>Total a pagar:</p>
             <span>${totalPrice.toFixed(2)}</span>
           </div>
           <button
-            title='Proceder con la compra y elegir método de pago.'
-            className='w-full mt-2 bg-sky-300/60 rounded-md py-2 font-semibold hover:bg-yellow-400 hover:scale-105 transition-transform'
+            title={
+              cartEmpty
+                ? "Debes agregar productos al carrito para poder realizar el pago"
+                : "Proceder con la compra y elegir método de pago."
+            }
+            className='confirm-and-pay-button w-full mt-2 bg-sky-300/60 rounded-md py-2 font-semibold hover:bg-yellow-400 hover:scale-105 transition-transform'
             onClick={openModal}
+            disabled={cartEmpty}
           >
             Confirmar y pagar
           </button>
