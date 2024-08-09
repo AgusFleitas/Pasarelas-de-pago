5902297ac1fcdccb9b7b52a98ef7232c1a63253e
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-08-09 20:15:22 +0200

    Update styles of payment views

--- client/src/views/PayFailed.jsx
+++ client/src/views/PayFailed.jsx
@@ -1,15 +1,16 @@
+import { Link } from 'react-router-dom'
+
 import error from '../img/error.webp'
 
 const PayFailed = () => {
     return (
         <section className="flex flex-col justify-center items-center">
             <h1 className="text-5xl font-bold mb-8">Pago no realizado</h1>
-            <div className="border-2 border-emerald-700 rounded-md w-96 flex flex-col justify-center items-center py-4 p-8 shadow-md shadow-slate-700">
+            <div className="border-2 bg-white border-sky-300/60 rounded-md w-96 flex flex-col justify-center items-center py-4 p-8 shadow-md shadow-gray-400">
                 <strong className="text-2xl mb text-red-600">¡Oh no!</strong>
                 <p>Ha habido un error al concretar el pago.</p>
                 <img className="my-6" src={error} alt="Ilustración de una equis roja dentro de un círculo que indica un error." />
-                <a className="bg-emerald-700/70 rounded-md py-1 px-3 w-32 text-center font-semibold mb-2 hover:scale-105 transition-transform" href="#">Ver detalles</a>
-                <a className="border-2 border-emerald-700/70 rounded-md py-1 px-3 w-32 text-center font-semibold hover:scale-105 transition-transform" href="/">Ir al inicio</a>
+                <Link className="bg-sky-300/60 rounded-md py-1 px-3 w-32 text-center font-semibold hover:bg-sky-400/60 transition-colors" to="/">Ir al inicio</Link>
             </div>
         </section>
     )
