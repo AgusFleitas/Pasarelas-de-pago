16434a656ed6c2cc454085291cd11638efddf464
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-08-14 19:17:04 +0200

    Change styles of paymentID because Stripe ID is too long

--- client/src/views/PaySuccess.jsx
+++ client/src/views/PaySuccess.jsx
@@ -1,12 +1,9 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-import useCart from "../hooks/useCart";
 import success from "../img/success.webp";
 
 const PaySuccess = () => {
-  const { retrieveSessionWithStripe } = useCart();
-
   // Creo un elemento URLSearchParams para obtener la query.
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
@@ -18,22 +15,18 @@ const PaySuccess = () => {
   const [paymentID, setPaymentID] = useState(null);
 
   useEffect(() => {
-    const retrieveSession = async (sessionID) => {
-      setPaymentID(await retrieveSessionWithStripe(sessionID))
-    };
-
-    if (query.get("session_id")) {
-      const sessionID = query.get("session_id");
-
-      retrieveSession(sessionID);
-    } setPaymentID(query.get("payment_id"));
+    if (query.has("session_id")) {
+      setPaymentID(query.get("session_id"));
+    } else if (query.has("payment_id")) {
+      setPaymentID(query.get("payment_id"));
+    }
   }, []);
 
   return (
     <section className='flex flex-col justify-center items-center'>
       <h1 className='text-5xl font-bold mb-8'>Pago realizado correctamente</h1>
       <div className='border-2 bg-white border-sky-300/60 rounded-md w-96 flex flex-col justify-center items-center py-4 p-8 shadow-md shadow-gray-400'>
-        <strong className='text-2xlç'>¡Felicidades!</strong>
+        <strong className='text-2xl'>¡Felicidades!</strong>
         <p>Tu pago ha sido aprobado de forma exitosa.</p>
         <img
           className='my-6'
@@ -41,9 +34,12 @@ const PaySuccess = () => {
           alt="Ilustración del símbolo 'correcto' que representa el pago con éxito."
         />
         {paymentID && (
-          <div className='flex flex-col items-center gap-y-2 mb-8'>
-            <div className='flex flex-col items-center text-xl gap-y-1'>
-              Payment ID: <strong className="bg-sky-200/50 py-1 px-2 rounded-md">{paymentID}</strong>
+          <div className='w-full flex flex-col items-center gap-y-2 mb-8'>
+            <div className='w-full flex flex-col items-center text-xl gap-y-1'>
+              <p>Payment ID:</p>
+              <p className='w-full bg-sky-200/50 py-1 px-2 rounded-md text-base text-center break-words'>
+                {paymentID}
+              </p>
             </div>
             <p className='text-center text-sm text-pretty'>
               Este código te servirá para consultar información sobre el pago
