b985b4ef77392c38ba3483b5dd4646f8aa48123e
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-08-16 19:44:44 +0200

    Re-styling + Responsive

--- client/src/views/PayPending.jsx
+++ client/src/views/PayPending.jsx
@@ -1,15 +1,15 @@
+import { Link } from 'react-router-dom'
 import warning from '../img/warning.webp'
 
 const PayPending = () => {
     return (
         <section className="flex flex-col justify-center items-center">
-            <h1 className="text-5xl font-bold mb-8">Pago pendiente</h1>
-            <div className="border-2 border-emerald-700 rounded-md w-96 flex flex-col justify-center items-center py-4 p-8 shadow-md shadow-slate-700">
-                <strong className="text-2xl mb text-yellow-500">¡Ya casi!</strong>
-                <p className='text-center'>Tu pago está pendiente, será aprobado una vez recibamos la transacción.</p>
-                <img className="my-6" src={warning} alt="Ilustración del símbolo 'correcto' que representa el pago con éxito." />
-                <a className="bg-emerald-700/70 rounded-md py-1 px-3 w-32 text-center font-semibold mb-2 hover:scale-105 transition-transform" href="#">Ver detalles</a>
-                <a className="border-2 border-emerald-700/70 rounded-md py-1 px-3 w-32 text-center font-semibold hover:scale-105 transition-transform" href="/">Ir al inicio</a>
+            <h1 className="font-bold text-4xl text-center text-balance mb-8">Pago pendiente</h1>
+            <div className="border-2 bg-white border-sky-300/60 rounded-md md:w-96 flex flex-col justify-center items-center py-4 p-8 shadow-md shadow-gray-400">
+                <strong className="text-2xl mb-2 text-yellow-500">Pago pendiente</strong>
+                <p className='text-center text-pretty'>Tu pago está pendiente, será aprobado una vez recibamos la transacción.</p>
+                <img className="my-6 opacity-70" src={warning} alt="Ilustración de un símbolo de advertencia que indica un pago pendiente." />
+                <Link className="bg-sky-300/60 rounded-md py-1 px-3 w-32 text-center font-semibold hover:bg-sky-400/60 transition-colors" to="/">Ir al inicio</Link>
             </div>
         </section>
     )
